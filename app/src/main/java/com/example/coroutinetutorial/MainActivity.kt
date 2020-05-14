547a8efe7ebb29fc6d471671398de12f30f0744e
Rohit Kumar <rkumar8092378845@gmail.com>
2020-05-14 11:05:18 +0530

    Coroutine - Deffered-Async-Await(Successive or sequential)

--- app/src/main/java/com/example/coroutinetutorial/MainActivity.kt
+++ app/src/main/java/com/example/coroutinetutorial/MainActivity.kt
@@ -7,13 +7,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
-    private companion object {
-
-        const val JOB_TIMEOUT = 1900L
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,52 +26,57 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    /**
+     * Comparison between async/await and job/launch patterns.
+     * Major difference is async/await can return a value wrapped in a Deferred type.
+     */
+
     private suspend fun fakeApiRequest() {
 
-        coroutineScope {
+        withContext(IO) {
 
-            val job = launch {
-                try {
+            val executionTime = measureTimeMillis {
 
-                    val result1 = getResult_1_from_Api() // wait until job is done
-                    if (result1 == "Result #1") {
-                        setTextOnMainThread("Got $result1")
-                        val result2 = getResult_2_from_Api()  // wait until job is done
-
-                        if (result2 == "Result #2") {
-                            setTextOnMainThread("Got $result2")
-
-                        } else {
-                            setTextOnMainThread("Couldn't get Result #2")
-                        }
-
-                    } else {
-                        setTextOnMainThread("Couldn't get Result #1")
-
-                    }
-
-
-                } catch (e: CancellationException) {
-                    println("debug: CancellationException: ${e.message}")
-                    showToast(e.message.toString())
-
-                } finally {
-                    println("debug: Finishing job. Cleaning up resources...")
+                /*//Classic job/launch
+                var result1 = ""
+                val job1= launch {
+                    println("debug: launching job1: ${Thread.currentThread().name}")
+                    result1 = getResult_1_from_Api()
                 }
+                job1.join()
+
+               var result2 = ""
+               val job2 = launch {
+
+                   println("debug: launchinf job2: ${Thread.currentThread().name}")
+                   result2 = getResult_2_from_Api(result1)
+               }*/
+
+                //Using async/await   (preferred type)
+
+                val result1 = async {
+                    println("debug: launching job1: ${Thread.currentThread().name}")
+
+                    getResult_1_from_Api()
+                }.await()  //wait for the result 1
+
+                println("Got result1: $result1")
+
+                val result2 = async {
+
+                    println("debug: launching job1: ${Thread.currentThread().name}")
+
+                    getResult_2_from_Api(result1)
+                }.await()
+                println("Got result2: $result2")
 
             }
-            delay(JOB_TIMEOUT)
-            if(job.isActive){
-                job.cancel(CancellationException("Cancelling job...Job took longer than $JOB_TIMEOUT"))
-                job.join()
-                println("debug: Cancelling job...Job took longer than $JOB_TIMEOUT")
-            }
+            println("job1 and job2 are complete. It took $executionTime ms")
+            setTextOnMainThread("job1 and job2 are complete. It took $executionTime ms")
         }
-
-
     }
 
-    private fun setNewtext(input: String) {
+    private fun setNewText(input: String) {
 
         val newText = textView.text.toString() + "\n$input"
         textView.text = newText
@@ -84,11 +86,11 @@ class MainActivity : AppCompatActivity() {
     private suspend fun setTextOnMainThread(input: String) {
 
         withContext(Main) {
-            setNewtext(input)
+            setNewText(input)
         }
     }
 
-    private fun isJobRunning(isActive: Boolean) {
+    /*private fun isJobRunning(isActive: Boolean) {
 
         if (isActive) {
             showToast("Job is Active")
@@ -96,7 +98,7 @@ class MainActivity : AppCompatActivity() {
             showToast("Job is not Active")
         }
 
-    }
+    }*/
 
     private suspend fun getResult_1_from_Api(): String {
 
@@ -105,10 +107,14 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private suspend fun getResult_2_from_Api(): String {
+    private suspend fun getResult_2_from_Api(result1: String): String {
 
-        delay(1000)
-        return "Result #2"
+        delay(1700)
+        if (result1 == "Result #1") {
+            return "Result #2"
+
+        }
+        return "Result #1 was incorrect"
     }
 
 
